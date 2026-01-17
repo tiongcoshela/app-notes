13c502e737fec693682fb6ff8819ebe1dc70e698
Shela Marie Tiongco <nsmananquil.student@asiancollege.edu.ph>
2026-01-17 10:36:18 +0800

    Confirmed SDK 54 compatibility fixes

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // ... other plugins if you have them
+      'react-native-reanimated/plugin', 
     ],
   };
 };
